6a175bc43456a7b1335a60a99b070a5ecf77b4db
barronbytes <jonathan.m.barron@gmail.com>
2025-07-23 15:00:55 -0400

    Client-response setup

--- main.py
+++ main.py
@@ -1,5 +1,33 @@
+import sys
+import os
+from dotenv import load_dotenv
+from google import genai
+from google.genai import types
+
+
+if len(sys.argv) == 1:
+    print("Error: No prompt argument provided.")
+    sys.exit(1)
+
+
+load_dotenv()
+api_key = os.environ.get("GEMINI_API_KEY")
+client = genai.Client(api_key=api_key)
+user_prompt = sys.argv[1]
+messages = [
+    types.Content(role="user", parts=[types.Part(text=user_prompt)]),
+]
+response = client.models.generate_content(
+    model='gemini-2.0-flash-001',
+    contents=messages
+)
+
+
 def main():
-    print("Hello from ai-agent!")
+
+    print(response.text)
+    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
 
 
 if __name__ == "__main__":
