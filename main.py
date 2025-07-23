9ccc99e6605783eae6bbf4c177bea63fa7a4a864
barronbytes <jonathan.m.barron@gmail.com>
2025-07-23 16:15:44 -0400

    Verbose setting

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ if len(sys.argv) == 1:
     sys.exit(1)
 
 
-def generate_content(client, messages):
+def generate_response(client, messages):
     response = client.models.generate_content(
         model='gemini-2.0-flash-001',
         contents=messages,
@@ -20,8 +20,8 @@ def generate_content(client, messages):
     return response
 
 
-def response_output(response, user_prompt, final_prompt):
-    if final_prompt == "--verbose":
+def generate_content(response, user_prompt, is_verbose):
+    if is_verbose:
         print(f"User prompt: {user_prompt}")
         print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
         print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
@@ -35,13 +35,13 @@ def main():
     api_key = os.environ.get("GEMINI_API_KEY")
     client = genai.Client(api_key=api_key)
     user_prompt = " ".join(args)
-    final_prompt = args[-1]
+    is_verbose = bool(args[-1] == "--verbose")
     messages = [
         types.Content(role="user", parts=[types.Part(text=user_prompt)]),
     ]
 
-    response = generate_content(client, messages)
-    response_output(response, user_prompt, final_prompt)
+    response = generate_response(client, messages)
+    generate_content(response, user_prompt, is_verbose)
 
 
 if __name__ == "__main__":
