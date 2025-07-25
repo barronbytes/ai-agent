a2d1d4af185086123698da3f3d770af18ec0777e
barronbytes <jonathan.m.barron@gmail.com>
2025-07-25 11:55:34 -0400

    get_files_info: catch errors

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -2,6 +2,7 @@ import os
 
 
 def root_dir() -> str:
+    # Returns root path
     file_path = os.path.abspath(__file__)
     current_dir = os.path.dirname(file_path)
     root_dir = os.path.abspath(os.path.join(current_dir, ".."))
@@ -9,15 +10,16 @@ def root_dir() -> str:
 
 
 def get_contents(working_directory, directory):
+    # Returns directory contents if path originals from root path, otherwise returns error message
     try:
         full_path = os.path.join(working_directory, directory)
-        if not os.path.abspath(full_path).startswith(os.path.abspath(working_directory)):
+        if not os.path.abspath(full_path).startswith(os.path.abspath(working_directory)): # valid path but not from root directory
             return f'Error: Cannot list "{directory}" as it is outside the permitted working directory'
-        if not os.path.isdir(full_path):
+        if not os.path.isdir(full_path): # valid path but not a directory (i.e., a file)
             return f'Error: "{directory}" is not a valid directory'
         return os.listdir(full_path)
     except Exception as e:
-        return f"Error: {str(e)}"
+        return f"Error: {str(e)}" # invalid path provided, etc.
 
 
 def get_files_info(working_directory, directory="."):
