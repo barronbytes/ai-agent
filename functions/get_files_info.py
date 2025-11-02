60d3bec4249365daa51deebae411f202c9d0ebe0
barronbytes <jonathan.m.barron@gmail.com>
2025-11-02 13:45:56 -0500

    Refactored get_files_info

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -2,46 +2,30 @@ import os
 from functions.utils import root_dir
 
 
-def get_contents(working_directory, directory):
-    # Returns the contents of a directory if it is within the root path; otherwise, returns an error message
-    try:
-        full_path = os.path.join(working_directory, directory)
-        if not os.path.abspath(full_path).startswith(os.path.abspath(working_directory)):
-            return f'Error: Cannot list "{directory}" as it is outside the permitted working directory'
-            # valid path but not from root directory
-            # safeguards against `traversal attacks` like `directory=..\..\..`
-        if not os.path.isdir(full_path): # valid path but not a directory (i.e., a file)
-            return f'Error: "{directory}" is not a valid directory'
-        return os.listdir(full_path)
-    except Exception as e:
-        return f"Error: {str(e)}" # invalid path provided, etc.
-
-
 def get_files_info(working_directory, directory="."):
-    contents = get_contents(working_directory, directory)
-    if isinstance(contents, str) and contents.startswith("Error:"):
-        return (contents, directory)
+    """
+    Returns files with metadata at the path: root_dir() + working_directory + directory
+    """
+    contents = []
+    try:
+        full_path = os.path.join(root_dir(), working_directory, directory)
+        # safeguard against directory traversal (e.g., directory=..\..\..)
+        if not os.path.abspath(full_path).startswith(os.path.abspath(root_dir())):
+            return f'Error: Cannot list "{directory}" as it is outside the permitted working directory'
+        if not os.path.isdir(full_path):
+            return f'Error: "{directory}" is not a valid directory'
 
-    lines = []
-    for item in contents:
-        try:
-            item_path = os.path.join(working_directory, directory, item)
+        contents = os.listdir(full_path)
+    except Exception as e:
+        return f"Error: {str(e)}"
+
+    try:
+        files_info = []
+        for item in contents:
+            item_path = os.path.join(full_path, item)
             file_size = os.path.getsize(item_path)
             is_dir = os.path.isdir(item_path)
-            lines.append(f"- {item}: file_size={file_size} bytes, is_dir={is_dir}")
-        except Exception as e:
-            lines.append(f"- {item}: Error: {str(e)}")
-    return (lines, directory)
-
-
-def generate_report(lines, directory):
-    message = ""
-    if isinstance(lines, str) and lines.startswith("Error:"):                                        
-        message = '\tError: Cannot list "{directory}" as it is outside the permitted working directory'
-    else:
-        message = "\n".join(lines)
-    return f"Result for '{directory}' directory:\n{message}"
-
-lines, directory = get_files_info(root_dir(), "calculator/pkg")
-message = generate_report(lines, directory)
-print(message)
+            files_info.append(f"- {item}: file_size={file_size} bytes, is_dir={is_dir}")
+        return "\n".join(files_info)
+    except Exception as e:
+            return f"- Error: Cannot list file: {str(e)}"
--- tests.py
+++ tests.py
@@ -1,21 +1,24 @@
 import os
-from functions.run_python_file import run_python_file
+from functions.get_files_info import get_files_info
 
 
 def test():
-    directory= "calculator"
-    t1 = run_python_file(directory, "main.py")
-    t2 = run_python_file(directory, "main.py", ["3 + 5"])
-    t3 = run_python_file(directory, "tests.py")
-    t4 = run_python_file(directory, "../main.py")
-    t5 = run_python_file(directory, "nonexistent.py")
-    t6 = run_python_file(directory, "lorem.txt")
-    print(t1)
-    print(t2)
-    print(t3)
-    print(t4)
-    print(t5)
-    print(t6)
+    result = get_files_info("calculator", ".")
+    print("Result for current directory:")
+    print(result)
+    print("")
+
+    result = get_files_info("calculator", "pkg")
+    print("Result for 'pkg' directory:")
+    print(result)
+
+    result = get_files_info("calculator", "/bin")
+    print("Result for '/bin' directory:")
+    print(result)
+
+    result = get_files_info("calculator", "../")
+    print("Result for '../' directory:")
+    print(result)
 
 
 if __name__ == "__main__":
