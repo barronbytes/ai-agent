9b8825bd98c8da8812957824ed2a23795c639efd
barronbytes <jonathan.m.barron@gmail.com>
2025-11-02 16:38:20 -0500

    Step 1+2 of function calling

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
 import sys # interact with Python runtime → interpreter settings, arguments, system-level information
 import os # interact with operating system → files, directories, environmental variables, processes
-from dotenv import load_dotenv  # type: ignore
+from dotenv import load_dotenv
 from google import genai
-from google.genai import types  # type: ignore
+from google.genai import types
+from functions.get_files_info import schema_get_files_info
 
 
 load_dotenv()
-AI_MODEL = "gemini-2.0-flash-001"
+API_KEY = os.getenv("GEMINI_API_KEY")
+AI_MODEL = os.getenv("AI_MODEL")
+SYSTEM_PROMPT = os.getenv("SYSTEM_PROMPT")
 
 
 if len(sys.argv) == 1:
@@ -23,37 +26,54 @@ def get_system_prompts():
     return [user_prompt, is_verbose]
 
 
-def generate_response(client, messages):
-    system_prompt=os.getenv("SYSTEM_PROMPT")
-    response = client.models.generate_content(
+def get_response(client, config, contents):
+    return client.models.generate_content(
         model=AI_MODEL,
-        contents=messages,
-        config=types.GenerateContentConfig(
-            system_instruction=system_prompt,
-        ),
+        config=config,
+        contents=contents,
     )
-    return response
 
 
-def generate_content(response, user_prompt, is_verbose):
+def print_report(response, user_prompt, is_verbose):
     if is_verbose:
         print(f"User prompt: {user_prompt}")
         print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
         print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
-    print("Response:")
-    print(response.text)
+
+    
+    if not response.function_calls:
+        print("Response:")
+        print(response.text)
+    else:
+        for function_call_part in response.function_calls:
+            print(f"Calling function: {function_call_part.name}({function_call_part.args})")
 
 
 def main():
+    # Step #1: Define a function declaration (done in functions.get_files_info)
+    # Step #2: Call the model with function declarations
+
+    # Configure the client and tools
+    client = genai.Client(api_key=API_KEY)
+    tools = types.Tool(
+        # Available functions
+        function_declarations=[
+            schema_get_files_info,
+        ]
+    )
+    config=types.GenerateContentConfig(
+        tools=[tools], system_instruction=SYSTEM_PROMPT
+    )
+
+    # Define user prompt
     user_prompt, is_verbose = get_system_prompts()
-    api_key = os.getenv("GEMINI_API_KEY")
-    client = genai.Client(api_key=api_key)
-    messages = [
+    contents = [
         types.Content(role="user", parts=[types.Part(text=user_prompt)]),
     ]
 
-    response = generate_response(client, messages)
-    generate_content(response, user_prompt, is_verbose)
+    # Send request with function declarations
+    response = get_response(client, config, contents)
+    print_report(response, user_prompt, is_verbose)
 
 
 if __name__ == "__main__":
